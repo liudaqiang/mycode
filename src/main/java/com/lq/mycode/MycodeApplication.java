84abf5507ef4b6499f55dc9c20697bf125e66842
827244716@qq.com <827244716@qq.com>
2018-01-05 15:40:42 +0800

    测试提交

---
+++ src/main/java/com/lq/mycode/MycodeApplication.java
@@ -0,0 +1,12 @@
+package com.lq.mycode;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MycodeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MycodeApplication.class, args);
+	}
+}
---
+++ src/main/java/com/lq/mycode/controller/DemoController.java
@@ -0,0 +1,12 @@
+package com.lq.mycode.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class DemoController {
+	@RequestMapping("/demo")
+	public String demo(){
+		return "aa";
+	}
+}
