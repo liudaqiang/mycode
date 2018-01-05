0dfcf7408c5436ce98bb9a3628cb04c20d636e9d
827244716@qq.com <827244716@qq.com>
2018-01-05 15:55:23 +0800

    测试提交

--- src/main/java/com/lq/mycode/controller/DemoController.java
+++ src/main/java/com/lq/mycode/controller/DemoController.java
@@ -2,7 +2,7 @@ package com.lq.mycode.controller;
 
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-
+//这是一次测试的提交
 @RestController
 public class DemoController {
 	@RequestMapping("/demo")
